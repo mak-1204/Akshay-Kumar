2f299eaa1ee4c9c4601f662efe067edeab1e886d
Akshay Kumar <akshay.muralibabu@gmail.com>
2026-03-19 15:11:23 +0000

    use this font - Typekiln (_for element h1_)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ export default {
         body: ['Inter', 'sans-serif'],
         headline: ['Inter', 'sans-serif'],
         code: ['Fira Code', 'monospace'],
-        display: ['Eloquia', 'sans-serif'],
+        display: ['Typekiln', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',

6a313889049fbfd26146de1f98bc11d874182890
Akshay Kumar <akshay.muralibabu@gmail.com>
2026-03-20 13:03:06 +0000

    spotlight effect shd appear if i touch the svcreen while usining itin my

--- src/components/GlowEffect.tsx
+++ src/components/GlowEffect.tsx
@@ -4,13 +4,30 @@ import { useEffect } from 'react';
 
 export function GlowEffect() {
   useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      document.documentElement.style.setProperty('--cx', e.clientX + 'px');
-      document.documentElement.style.setProperty('--cy', e.clientY + 'px');
+    const updateGlow = (x: number, y: number) => {
+      document.documentElement.style.setProperty('--cx', x + 'px');
+      document.documentElement.style.setProperty('--cy', y + 'px');
     };
 
-    document.addEventListener('mousemove', handleMouseMove);
-    return () => document.removeEventListener('mousemove', handleMouseMove);
+    const handleMouseMove = (e: MouseEvent) => {
+      updateGlow(e.clientX, e.clientY);
+    };
+
+    const handleTouch = (e: TouchEvent) => {
+      if (e.touches && e.touches[0]) {
+        updateGlow(e.touches[0].clientX, e.touches[0].clientY);
+      }
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('touchstart', handleTouch, { passive: true });
+    window.addEventListener('touchmove', handleTouch, { passive: true });
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('touchstart', handleTouch);
+      window.removeEventListener('touchmove', handleTouch);
+    };
   }, []);
 
   return <div className="cursor-glow" />;
